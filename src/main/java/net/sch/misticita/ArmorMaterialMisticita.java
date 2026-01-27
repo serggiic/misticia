5c0ebd67527e96c1b0fec109c6e7ca1a2925c921
duustixx <justinklk866@gmail.com>
2026-01-28 00:46:22 +0100

    Pala añadida y código cambiado

--- src/main/java/net/sch/misticita/ArmorMaterialMisticita.java
+++ src/main/java/net/sch/misticita/ArmorMaterialMisticita.java
@@ -1,65 +1,72 @@
 package net.sch.misticita;
 
+import net.minecraft.Util;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.LazyLoadedValue;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.EnumMap;
 import java.util.function.Supplier;
 
 public enum ArmorMaterialMisticita implements ArmorMaterial {
-    MISTICITA("misticita:misticita", 45, new int[]{4, 7, 9, 4}, 20,
-            SoundEvents.ARMOR_EQUIP_NETHERITE, 4.0f, 0.2f,
+    MISTICITA("misticita", 45, Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 4);
+        map.put(ArmorItem.Type.LEGGINGS, 7);
+        map.put(ArmorItem.Type.CHESTPLATE, 9);
+        map.put(ArmorItem.Type.HELMET, 4);
+    }), 20, SoundEvents.ARMOR_EQUIP_NETHERITE, 4.0f, 0.2f,
             () -> Ingredient.of(Misticita.MISTICITA_INGOT.get()));
 
-    private static final java.util.Map<ArmorItem.Type, Integer> HEALTH_PER_SLOT = java.util.Map.of(
-            ArmorItem.Type.HELMET, 13,
-            ArmorItem.Type.CHESTPLATE, 15,
-            ArmorItem.Type.LEGGINGS, 16,
-            ArmorItem.Type.BOOTS, 11
-    );
+    private static final EnumMap<ArmorItem.Type, Integer> HEALTH_FUNCTION_FOR_TYPE = Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 13);
+        map.put(ArmorItem.Type.LEGGINGS, 15);
+        map.put(ArmorItem.Type.CHESTPLATE, 16);
+        map.put(ArmorItem.Type.HELMET, 11);
+    });
+
     private final String name;
     private final int durabilityMultiplier;
-    private final int[] slotProtections;
+    private final EnumMap<ArmorItem.Type, Integer> protectionFunctionForType;
     private final int enchantmentValue;
     private final SoundEvent sound;
     private final float toughness;
     private final float knockbackResistance;
-    private final LazyLoadedValue<Ingredient> repairIngredient;
+    private final Supplier<Ingredient> repairIngredient;
 
-    ArmorMaterialMisticita(String name, int durabilityMultiplier, int[] slotProtections,
-                        int enchantmentValue, SoundEvent sound, float toughness,
-                        float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+    ArmorMaterialMisticita(String name, int durabilityMultiplier, EnumMap<ArmorItem.Type, Integer> protectionFunctionForType,
+                           int enchantmentValue, SoundEvent sound, float toughness,
+                           float knockbackResistance, Supplier<Ingredient> repairIngredient) {
         this.name = name;
         this.durabilityMultiplier = durabilityMultiplier;
-        this.slotProtections = slotProtections;
+        this.protectionFunctionForType = protectionFunctionForType;
         this.enchantmentValue = enchantmentValue;
         this.sound = sound;
         this.toughness = toughness;
         this.knockbackResistance = knockbackResistance;
-        this.repairIngredient = new LazyLoadedValue<>(repairIngredient);
+        this.repairIngredient = repairIngredient;
     }
-    @Override
-    public SoundEvent getEquipSound() {
-        return this.sound;
-    }
+
     @Override
     public int getDurabilityForType(ArmorItem.Type type) {
-        return HEALTH_PER_SLOT.get(type) * this.durabilityMultiplier;
+        return HEALTH_FUNCTION_FOR_TYPE.get(type) * this.durabilityMultiplier;
     }
 
     @Override
     public int getDefenseForType(ArmorItem.Type type) {
-        return this.slotProtections[type.ordinal()];
+        return this.protectionFunctionForType.get(type);
     }
 
     @Override
     public int getEnchantmentValue() {
         return this.enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return this.sound;
     }
 
     @Override
--- src/main/java/net/sch/misticita/Misticita.java
+++ src/main/java/net/sch/misticita/Misticita.java
@@ -1,7 +1,13 @@
 package net.sch.misticita;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
@@ -15,10 +21,18 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+
 @Mod(Misticita.MODID)
 public class Misticita {
     public static final String MODID = "misticita";
 
+    //Pestaña creativa personalizada
+    public static final CreativeModeTab MISTICITA_TAB = new CreativeModeTab("misticita_tab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(MISTICITA_INGOT.get());
+        }
+    };
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
@@ -26,6 +40,7 @@ public class Misticita {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
+    // ========== BLOQUES ==========
     // El Bloque de Mineral
     public static final RegistryObject<Block> MISTICITA_ORE = BLOCKS.register("misticita_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)
@@ -35,9 +50,10 @@ public class Misticita {
 
     // Registramos el bloque como un Ítem para que aparezca en el inventario
     public static final RegistryObject<Item> MISTICITA_ORE_ITEM = ITEMS.register("misticita_ore",
-            () -> new BlockItem(MISTICITA_ORE.get(), new Item.Properties()));
+            () -> new BlockItem(MISTICITA_ORE.get(),
+                    new Item.Properties().tab(MISTICITA_TAB)));
 
-    // El Bloque de depslate
+    // El Bloque de Deepslate
     public static final RegistryObject<Block> DEEPSLATE_MISTICITA_ORE = BLOCKS.register("deepslate_misticita_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)
                     .strength(4.5f)
@@ -47,7 +63,7 @@ public class Misticita {
     // El Ítem del bloque
     public static final RegistryObject<Item> DEEPSLATE_MISTICITA_ORE_ITEM = ITEMS.register("deepslate_misticita_ore",
             () -> new BlockItem(DEEPSLATE_MISTICITA_ORE.get(), 
-                new Item.Properties()));
+                new Item.Properties().tab(MISTICITA_TAB)));
 
     //Bloque de misticita
     public static final RegistryObject<Block> MISTICITA_BLOCK = BLOCKS.register("misticita_block",
@@ -59,7 +75,7 @@ public class Misticita {
     //Ítem del bloque de misticita
     public static final RegistryObject<Item> MISTICITA_BLOCK_ITEM = ITEMS.register("misticita_block",
         () -> new BlockItem(MISTICITA_BLOCK.get(),
-                new Item.Properties()));
+                new Item.Properties().tab(MISTICITA_TAB).fireResistant()));
 
     //Ladrillos de Misticita
     public static final RegistryObject<Block> MISTICITA_BRICKS = BLOCKS.register("misticita_bricks",
@@ -70,12 +86,14 @@ public class Misticita {
     //Ítem de los ladrillos de Misticita
     public static final RegistryObject<Item> MISTICITA_BRICKS_ITEM = ITEMS.register("misticita_bricks",
         () -> new BlockItem(MISTICITA_BRICKS.get(),
-                new Item.Properties()));
+                new Item.Properties().tab(MISTICITA_TAB).fireResistant()));
 
-    
+    // ========== ÍTEMS ==========
+
     public static final RegistryObject<Item> MISTICITA_INGOT = ITEMS.register("misticita_ingot",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().tab(MISTICITA_TAB).fireResistant()));
 
+    // ========== ARMADURA ==========
     public static final RegistryObject<Item> MISTICITA_HELMET = ITEMS.register("misticita_helmet",
             () -> new ArmorItem(ArmorMaterialMisticita.MISTICITA, ArmorItem.Type.HELMET, new Item.Properties()));
 
@@ -95,9 +113,20 @@ public class Misticita {
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
     }
+
+    // ========== HERRAMIENTAS ==========
     // Espada
     public static final RegistryObject<Item> MISTICITA_SWORD = ITEMS.register("misticita_sword",
-            () -> new SwordItem(Tiers.NETHERITE, 8, -2.4f, new Item.Properties()));
+            () -> new SwordItem(Tiers.NETHERITE, 8, -2.4f, new Item.Properties().tab(MISTICITA_TAB).fireResistant()) {
+            @Override
+                public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+                target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 60, 1));
+                if (attacker instanceof Player player) {
+                    player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 40, 0));
+                }
+                return super.hurtEnemy(stack, target, attacker);
+            }
+            });
 
     // Pico
     public static final RegistryObject<Item> MISTICITA_PICKAXE = ITEMS.register("misticita_pickaxe",
@@ -107,10 +136,47 @@ public class Misticita {
                     // Si el bloque es picable con este pico, devolvemos 11.0f (Netherite es 9.0f)
                     return super.getDestroySpeed(stack, state) > 1.0f ? 11.0f : 1.0f;
                 }
+
+                @Override
+                public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miner) {
+                    if(!level.isClientSide && state.getDestroySpeed(level, pos) != 0.0F) {
+                        if(miner instanceof Player player) {
+                            player.giveExperiencePoints(1);
+                        }
+                    }
+                    return super.mineBlock(stack, level, state, pos, miner);
+                }
             });
 
     // Hacha
     public static final RegistryObject<Item> MISTICITA_AXE = ITEMS.register("misticita_axe",
-            () -> new AxeItem(Tiers.NETHERITE, 10, -3.0f, new Item.Properties()));
+            () -> new AxeItem(Tiers.NETHERITE, 10, -3.0f,
+                    new Item.Properties().tab(MISTICITA_TAB).fireResistant()) {
+            @Override
+                public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 
+
+                target.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 100, 0));
+                return super.hurtEnemy(stack, target, attacker);
+            }
+
+            @Override
+                public float getDestroySpeed(ItemStack stack, BlockState state) {
+                return super.getDestroySpeed(stack, state) > 1.0f ? 10.0f : 1.0f;
+                }
+            });
+
+    //Pala
+    public static final RegistryObject<Item> MISTICITA_SHOVEL = ITEMS.register("misticita_shovel",
+            () -> new ShovelItem(Tiers.NETHERITE, 1.5f, -3.0f,
+                new Item.Properties().tab(MISTICITA_TAB).fireResistant()) {
+
+                @Override
+                public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miner) {
+                if(state.is(Blocks.DIRT) || state.is(Blocks.GRASS_BLOCK)) {
+                    level.setBlock(pos, Blocks.GRASS_BLOCK.defaultBlockState(), 3);
+                }
+                return super.mineBlock(stack, level, state, pos, miner);
+        }
+    });
 }
